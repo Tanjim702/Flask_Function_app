9289f7502c16b9d3a7e0c6b2cb6e68c464d5d49f
Your Name <you@example.com>
2021-01-12 17:40:59 +0000

    third commit

--- series.py
+++ series.py
@@ -94,5 +94,6 @@ def result():
                             isLong=isLong)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
